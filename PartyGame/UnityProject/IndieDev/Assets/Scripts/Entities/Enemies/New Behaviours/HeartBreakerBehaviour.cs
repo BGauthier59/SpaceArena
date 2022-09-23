db0dce1ff48811870db0b8645b7b3ceb294c47ce
BGauthier <gauthierbaudinet@gmail.com>
2022-09-23 16:01:57 +0200

    Walls & arenas

--- PartyGame/UnityProject/IndieDev/Assets/Scripts/Entities/Enemies/New Behaviours/HeartBreakerBehaviour.cs
+++ PartyGame/UnityProject/IndieDev/Assets/Scripts/Entities/Enemies/New Behaviours/HeartBreakerBehaviour.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HeartBreakerBehaviour : BreakerBehaviour
 {
+    
+    // N'hérite pas de BreakerBehaviour
+    
     public override void Target()
     {
         foreach (var element in BaseManager.instance.allBaseElements)
@@ -11,6 +15,12 @@ public class HeartBreakerBehaviour : BreakerBehaviour
             if (element.GetType() == typeof(BaseHeart)) target = element;
         }
         // Target le coeur
+
+        if (!IsTargetAvailable())
+        {
+            Debug.Log("HeartBreaker can't reach the Heart");
+            SwitchState(BreakerState.Idle);
+        }
     }
 
     public override void Attack()
