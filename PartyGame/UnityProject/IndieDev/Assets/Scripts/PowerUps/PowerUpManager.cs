4e82db1c4cd21e0615ce661429dc56b408b83488
ElSachs <75885665+ElSachs@users.noreply.github.com>
2022-10-06 13:39:14 +0200

    oui

--- PartyGame/UnityProject/IndieDev/Assets/Scripts/PowerUps/PowerUpManager.cs
+++ PartyGame/UnityProject/IndieDev/Assets/Scripts/PowerUps/PowerUpManager.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpManager : MonoBehaviour
 {
     public PlayerController user;
+    public PowerUps powerUp;
+    public Dictionary<PowerUps, PowerUpManager> powerUpScript;
 
-    public void Start()
+    public enum PowerUps
     {
-        OnUse();
+        Shotgun, Laser, Turret, GrenadeLauncher, Rage, Tesla, Overload, Wormhole 
+    }
+
+    public virtual void OnActivate()
+    {
+        
     }
 
     public virtual void OnUse()
@@ -15,8 +23,50 @@ public class PowerUpManager : MonoBehaviour
         
     }
     
-    public virtual void OnConditionCheck()
+    public virtual bool OnConditionCheck()
     {
-        
+        return true;
     }
 }
+
+public class ShotgunBehaviour : PowerUpManager
+{
+    
+}
+
+public class LaserBehaviour : PowerUpManager
+{
+    
+}
+
+public class TurretBehaviour : PowerUpManager
+{
+    
+}
+
+public class GrenadeLauncherBehaviour : PowerUpManager
+{
+    
+}
+
+public class RageBehaviour : PowerUpManager
+{
+    
+}
+
+public class TeslaBehaviour : PowerUpManager
+{
+    
+}
+
+public class OverloadBehaviour : PowerUpManager
+{
+    
+}
+
+public class WormholeBehaviour : PowerUpManager
+{
+    
+}
+
+
