488797d92691e2c00580853f2e8fa4f4d9f41efb
BGauthier59 <g.baudinet@rubika-edu.com>
2023-01-05 17:33:25 +0100

    grrrrrr uwuuuuuuuuu

---
+++ PartyGame/UnityProject/IndieDev/Assets/Concurrence/MeilleurJeu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeilleurJeu : MonoBehaviour
+{
+    /*
+     
+     Ghost at Ikea est un bien meilleur jeu, frenchement, vous devriez avoir honte de faire un jeu aussi nul comparé a Ghost At Ikea.
+     
+     Si vous voulez vous faire pardonnez, donnez 10 000 euros a Mathis Jibert en classe de B3GD !!! 
+     
+     Attention, vos jours sont comptés !
+
+     */
+}
